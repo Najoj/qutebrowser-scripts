ec773d71ea83603aac5e92dbcda3fc0dd2fe6fda
Johan Öhlin <johanohlin@gmx.com>
2019-11-24 10:45:35 +0100

    Whitelisting now works
    The sed with http(s)? was not needed, as the cut removed those.
    
    Now two cut is used in order to get domain and top-level domain.

--- whitelist.sh
+++ whitelist.sh
@@ -4,10 +4,10 @@
 FILE=/home/${USER}/.config/qutebrowser-cookiecleaner.list
 TMP=${FILE}.tmp
 # Host
-HOST=$(echo $QUTE_URL | cut -d\/ -f3 | sed "s/^http(s)?\:\/\//d")
+HOST=$(echo $QUTE_URL | cut -d\/ -f3 | cut -d\. -f2-3)
 # Whitelist it
 echo $HOST >> $FILE
 sort -u $FILE > $TMP
 cp $TMP $FILE
 # Inform user
-echo "message-info \"added ${HOST} to whitelist\"" >> ${QUTE_FIFO}
+echo "message-info \"added '${HOST}' to whitelist\"" >> ${QUTE_FIFO}
