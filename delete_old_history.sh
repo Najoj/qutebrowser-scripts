095daaa7d8873e546c1cbe69c3cf2db788ab60c6
Johan Öhlin <johanohlin@gmx.com>
2019-11-24 10:47:50 +0100

    Clean-up in delete_old_history script
    Clean-up in script. Now uses "debug" as first argument for debug.
    I.e. not executing command to remove, but only show the result to
    stdout.
    
    Re-arranged variables and if statement in script.

--- delete_old_history.sh
+++ delete_old_history.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 FILE="/home/${USER}/.local/share/qutebrowser/history.sqlite"
-TIME="$(date +%s --date="6 months ago")";
+TIME="$(date +%s --date="6 months ago")"
 
-CMD="delete from CompletionHistory where last_atime < "
-# CMD="select * from CompletionHistory where last_atime < "
+DBG="select * from CompletionHistory where last_atime < $TIME"
+CMD="delete from CompletionHistory where last_atime < $TIME"
 
-CMD="$CMD $TIME"
-sqlite3 ${FILE} "${CMD}"
-
-CMD="delete from History where atime < "
-if [ $# -gt 0 ]; then
-    CMD="select * from History where atime < "
+# if not debug, we'll execute command and only log
+if [ "$1" == "debug" ]; then
+        OUTPUT="/dev/stdout"
+        echo "DEBUG" > $OUTPUT
+else
+        OUTPUT="/home/${USER}/.qutebrowser-delete-history-log"
+        sqlite3 ${FILE} "${CMD}"
 fi
 
-CMD="$CMD $TIME"
-sqlite3 ${FILE} "${CMD}"
+sqlite3 ${FILE} "${DBG}" > $OUTPUT
 
 exit $?
