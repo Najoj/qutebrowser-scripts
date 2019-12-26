1a4c915a2150b4b25c351c091aa1aa53890e5571
Johan Öhlin <johanohlin@gmx.com>
2019-12-26 23:39:50 +0100

    Added double-quote for concise scripting
    Only because I had used double-quote signs in other scripts, I decided
    to use them in whitelist.sh as well.

--- whitelist.sh
+++ whitelist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Whitelist file
-FILE=/home/${USER}/.config/qutebrowser-cookiecleaner.list
-TMP=${FILE}.tmp
+FILE="/home/${USER}/.config/qutebrowser-cookiecleaner.list"
+TMP="${FILE}.tmp"
 # Host
-HOST=$(echo $QUTE_URL | cut -d\/ -f3 | sed "s/^www.//")
+HOST="$(echo $QUTE_URL | cut -d\/ -f3 | sed "s/^www.//")"
 # Whitelist it
 echo $HOST >> $FILE
 sort -u $FILE > $TMP
