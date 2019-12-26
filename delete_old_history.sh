f54994cdeca6c296a5f432b8f1ddfe9e438d3f35
Johan Öhlin <johanohlin@gmx.com>
2019-12-27 00:40:12 +0100

    Delete history bug fixed
    It did not work as intended, as it cleaned the database before logging
    which lines were to be removed. The log would always be empty this way.

--- delete_old_history.sh
+++ delete_old_history.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-FILE="/home/${USER}/.local/share/qutebrowser/history.sqlite"
+DATABASE="/home/${USER}/.local/share/qutebrowser/history.sqlite"
 TIME="$(date +%s --date="6 months ago")"
 
 DBG="select * from CompletionHistory where last_atime < $TIME"
 CMD="delete from CompletionHistory where last_atime < $TIME"
 
+OUTPUT="/home/${USER}/.qutebrowser-delete-history-log"
+
 # if not debug, we'll execute command and only log
 if [ "$1" == "debug" ]; then
         OUTPUT="/dev/stdout"
         echo "DEBUG" > $OUTPUT
+        sqlite3 ${DATABASE} "${DBG}" > $OUTPUT
 else
-        OUTPUT="/home/${USER}/.qutebrowser-delete-history-log"
-        sqlite3 ${FILE} "${CMD}"
+        sqlite3 ${DATABASE} "${DBG}" > $OUTPUT
+        sqlite3 ${DATABASE} "${CMD}"
 fi
-
-sqlite3 ${FILE} "${DBG}" > $OUTPUT
 
 exit $?
