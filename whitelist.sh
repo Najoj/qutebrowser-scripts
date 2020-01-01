61d74ad3c11a2b5ffda706575964433ddddc826c
Johan Öhlin <johanohlin@gmx.com>
2020-01-01 23:37:52 +0100

    Whitelist script can now remove host
    Can now remove a host from list of whitelisted hosts. Requires "delete"
    argument to script.

--- whitelist.sh
+++ whitelist.sh
@@ -5,9 +5,19 @@ FILE="/home/${USER}/.config/qutebrowser-cookiecleaner.list"
 TMP="${FILE}.tmp"
 # Host
 HOST="$(echo $QUTE_URL | cut -d\/ -f3 | sed "s/^www.//")"
-# Whitelist it
-echo $HOST >> $FILE
-sort -u $FILE > $TMP
+# Remove from or add to list
+if [ "$1" == "delete" ]; then
+        # Remove host from file
+        grep -v "^${HOST}$" $FILE > $TMP
+        # Inform user
+        MSG="\"removed '${HOST}' from whitelist\""
+else
+        # Whitelist it
+        echo $HOST >> $FILE
+        sort -u $FILE > $TMP
+        MSG="\"added '${HOST}' to whitelist\""
+fi
+# Save new file
 cp $TMP $FILE
 # Inform user
-echo "message-info \"added '${HOST}' to whitelist\"" >> ${QUTE_FIFO}
+echo "message-info $MSG" >> ${QUTE_FIFO}
